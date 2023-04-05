1d96e177550c2c930181f672548c140bb9b8fc33
Colette DeLizo <delizcol000@gmail.com>
2023-04-05 14:01:03 -0700

    added all weather Code

--- utils/WeatherCondition.js
+++ utils/WeatherCondition.js
@@ -1,13 +1,7 @@
 
 export const weatherCondition = {
-    63: {
-      color: '#005BEA',
-      title: 'Raining',
-      subtitle: 'Moderate',
-      icon: 'weather-rainy'
-    },
     0: {
-      color: '#f7b733',
+      color: '#f5f102',
       title: 'Sunny',
       subtitle: 'Clear Sky',
       icon: 'weather-sunny'
@@ -19,47 +13,147 @@ export const weatherCondition = {
       icon: 'weather-sunny'
     },
     2: {
-      color: '#f6b750',
+      color: '#8da1a0',
       title: 'Sun/Clouds',
       subtitle: 'Partly Cloudy',
       icon: 'weather-partly-cloudy'
     },
     3: {
-      color: '#616161',
+      color: '#8da1a0',
       title: 'Clouds',
       subtitle: 'Overcast',
       icon: 'weather-cloudy'
     },
-    95: {
-      color: '#616161',
-      title: 'A Storm is coming',
-      subtitle: 'Because Gods are angry',
-      icon: 'weather-lightning'
+    45: {
+      color: '#97bdbc',
+      title: 'Fog',
+      subtitle: "",
+      icon: 'weather-fog'
     },
-
-  
+    48: {
+      color: '#97bdbc',
+      title: 'Haze',
+      subtitle: '',
+      icon: 'weather-hazy'
+    },
+    51: {
+      color: '#63c7e6',
+      title: 'Drizzle',
+      subtitle: 'Light',
+      icon: 'weather-hail'
+    },
+    53: {
+      color: '#63c7e6',
+      title: 'Drizzle',
+      subtitle: 'Moderate',
+      icon: 'weather-hail'
+    },
+    55: {
+      color: '#63c7e6',
+      title: 'Drizzle',
+      subtitle: 'Heavy',
+      icon: 'weather-hail'
+    },
+    56: {
+      color: '#63c7e6',
+      title: 'Freezing Drizzle',
+      subtitle: 'Light',
+      icon: 'weather-hail'
+    },
+    57: {
+      color: '#63c7e6',
+      title: 'Freezing Drizzle',
+      subtitle: 'Heavy',
+      icon: 'weather-hail'
+    },
+    61: {
+      color: '#005BEA',
+      title: 'Raining',
+      subtitle: 'Light',
+      icon: 'weather-partly-rainy'
+    },
+    63: {
+      color: '#005BEA',
+      title: 'Raining',
+      subtitle: 'Moderate',
+      icon: 'weather-rainy'
+    },
+    65: {
+      color: '#005BEA',
+      title: 'Raining',
+      subtitle: 'Heavy',
+      icon: 'weather-pouring'
+    },
     71: {
       color: '#00d2ff',
       title: 'Snow',
-      subtitle: 'Get out and build a snowman for me',
+      subtitle: 'Light',
+      icon: 'weather-partly-snowy'
+    },
+    73: {
+      color: '#00d2ff',
+      title: 'Snow',
+      subtitle: 'Moderate',
       icon: 'weather-snowy'
     },
-    61: {
-      color: '#076585',
-      title: 'Drizzle',
-      subtitle: 'Partially raining...',
+    75: {
+      color: '#00d2ff',
+      title: 'Snow',
+      subtitle: 'Heavy',
+      icon: 'weather-snowy-heavy'
+    },
+    77: {
+      color: '#00d2ff',
+      title: 'Snow Grains',
+      subtitle: 'Moderate',
       icon: 'weather-hail'
     },
-    48: {
-      color: '#66A6FF',
-      title: 'Haze',
-      subtitle: 'Another name for Partial Raining',
-      icon: 'weather-hazy'
+    80: {
+      color: '#005BEA',
+      title: 'Rain Showers',
+      subtitle: 'Light',
+      icon: 'weather-partly-rainy'
     },
-    45: {
-      color: '#3CD3AD',
-      title: 'Mist',
-      subtitle: "Don't roam in forests!",
-      icon: 'weather-fog'
-    }
+    81: {
+      color: '#005BEA',
+      title: 'Rain Showers',
+      subtitle: 'Moderate',
+      icon: 'weather-rainy'
+    },
+    82: {
+      color: '#005BEA',
+      title: 'Rain Showers',
+      subtitle: 'Violent',
+      icon: 'weather-pouring'
+    },
+    85: {
+      color: '#00d2ff',
+      title: 'Snow Showers',
+      subtitle: 'Light',
+      icon: 'weather-snowy'
+    },
+    86: {
+      color: '#00d2ff',
+      title: 'Snow Showers',
+      subtitle: 'Heavy',
+      icon: 'weather-snowy-heavy'
+    },
+    95: {
+      color: '#616161',
+      title: 'Thunderstorm',
+      subtitle: 'Light/Moderate',
+      icon: 'weather-lightning'
+    },
+    96: {
+      color: '#616161',
+      title: 'Thunderstorm',
+      subtitle: 'Light Hail',
+      icon: 'weather-lightning'
+    },
+    99: {
+      color: '#616161',
+      title: 'Thunderstorm',
+      subtitle: 'Heavy Hail',
+      icon: 'weather-lightning-rainy'
+    },
   };
